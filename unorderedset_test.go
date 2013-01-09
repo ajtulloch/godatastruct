2b759a822f810627acb1f724d7f65f9db7a9bbd9
Andrew Tulloch <tulloch@fb.com>
2013-01-10 00:22:25 +0800

    GoFmt FTW

--- unorderedset_test.go
+++ unorderedset_test.go
@@ -1,40 +1,41 @@
 package godatastruct
 
 import (
-  . "launchpad.net/gocheck"
-  "testing"
+	. "launchpad.net/gocheck"
+	"testing"
 )
 
 // Hook up gocheck into the gotest runner.
 func Test(t *testing.T) { TestingT(t) }
 
 type MySuite struct{}
+
 var _ = Suite(&MySuite{})
 
 func setTests(c *C, s Set) {
-  c.Assert(s.Set(1), Equals, true)
-  c.Assert(s.Exists(1), Equals, true)
-  c.Assert(s.Exists(2), Equals, false)
-  c.Assert(s.Len(), Equals, 1)
-  s.Clear()
+	c.Assert(s.Set(1), Equals, true)
+	c.Assert(s.Exists(1), Equals, true)
+	c.Assert(s.Exists(2), Equals, false)
+	c.Assert(s.Len(), Equals, 1)
+	s.Clear()
 
-  c.Assert(s.Len(), Equals, 0)
-  c.Assert(s.Exists(1), Equals, false)
+	c.Assert(s.Len(), Equals, 0)
+	c.Assert(s.Exists(1), Equals, false)
 
-  c.Assert(s.Set(1), Equals, true)
-  c.Assert(s.Set("Cats"), Equals, true)
-  c.Assert(s.Len(), Equals, 2)
-  c.Assert(s.Erase("Cats"), Equals, true)
-  c.Assert(s.Erase("Dogs"), Equals, false)
-  c.Assert(s.Len(), Equals, 1)
+	c.Assert(s.Set(1), Equals, true)
+	c.Assert(s.Set("Cats"), Equals, true)
+	c.Assert(s.Len(), Equals, 2)
+	c.Assert(s.Erase("Cats"), Equals, true)
+	c.Assert(s.Erase("Dogs"), Equals, false)
+	c.Assert(s.Len(), Equals, 1)
 }
 
 func (s *MySuite) TestUnorderedSet(c *C) {
-  us := NewUnorderedSet() 
-  setTests(c, us)
+	us := NewUnorderedSet()
+	setTests(c, us)
 }
 
 func (s *MySuite) TestThreadSafeUnorderedSet(c *C) {
-  us:= NewTheadSafeUnorderedSet() 
-  setTests(c, us)
+	us := NewTheadSafeUnorderedSet()
+	setTests(c, us)
 }
